f9c98086108cda3b025d63bddcf871dde1a3d315
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-09-24 21:32:56 +0100

    Adding to program class

--- CodingTasks/Program.cs
+++ CodingTasks/Program.cs
@@ -1,5 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CodingTasks.FindTheMaxInt;
 using CodingTasks.SumEvenNumbersFromIntArray;
 
 int[] numbers = new int[] { 23, 22, 67, 89, 9, 76, 66 };
 Console.WriteLine("Sum of the even numbers in the array is : {0}", SumEvenNumbersFromIntArray.SumEven(numbers).ToString());
+
+FindTheMaxSum.MaxInt(15, 5);
