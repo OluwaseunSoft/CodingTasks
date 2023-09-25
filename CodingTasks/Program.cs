a374dd6fd5cf18ed995097dca352893b5e50eecc
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-09-25 21:29:38 +0100

    AlphabetPositionString

--- CodingTasks/Program.cs
+++ CodingTasks/Program.cs
@@ -3,6 +3,9 @@ using CodingTasks.FindTheMaxInt;
 using CodingTasks.SumEvenNumbersFromIntArray;
 
 int[] numbers = new int[] { 23, 22, 67, 89, 9, 76, 66 };
-Console.WriteLine("Sum of the even numbers in the array is : {0}", SumEvenNumbersFromIntArray.SumEven(numbers).ToString());
+//Console.WriteLine("Sum of the even numbers in the array is : {0}", SumEvenNumbersFromIntArray.SumEven(numbers).ToString());
 
-FindTheMaxSum.MaxInt(15, 5);
+//FindTheMaxSum.MaxInt(15, 5);
+
+int d = Convert.ToInt32('E');
+Console.WriteLine((d % 32).ToString());
