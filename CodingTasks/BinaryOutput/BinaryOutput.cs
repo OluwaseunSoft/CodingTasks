cb3f842e237df166387f0167a1c6e6a0798bf31f
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-09-29 21:36:22 +0100

    BinaryOutput

---
+++ CodingTasks/BinaryOutput/BinaryOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks.BinaryOutput
+{
+    internal class BinaryOutput
+    {
+        public static int[] BinaryFigure(int n)
+        {
+            int[] result = new int[n + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                int counter = 0;
+                string binary = Convert.ToString(i, 2);
+                var charBin = binary.ToCharArray();
+                foreach (var item in charBin)
+                {
+                    if (item == '1')
+                    {
+                        counter++;
+                    }
+                }
+                //var counter = binary.Where(x => x == '1').Count();
+                result[i] = counter;
+            }
+            return result;
+        }
+    }
+}
