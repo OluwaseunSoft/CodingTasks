8e659f9d78f6f064f26cc43b8ca32d30cda9f313
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-10-01 20:56:41 +0100

    OrderOfStrings

--- CodingTasks/OrderOfStringsAndNumbers/OrderOfStringsAndNumbers.cs
+++ CodingTasks/OrderOfStringsAndNumbers/OrderOfStringsAndNumbers.cs
@@ -8,5 +8,26 @@ namespace CodingTasks.OrderOfStringsAndNumbers
 {
     internal class OrderOfStringsAndNumbers
     {
+        public string OrderOfStrings(string s)
+        {
+            var space = s.Split(' ');
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            foreach (var item in space)
+            {
+                var num = item[item.Length - 1].ToString();
+                var subString = item.Substring(0, item.Length - 1);
+                dic.Add(Convert.ToInt16(num), subString);
+            }
+
+            var de = dic.OrderBy(k => k.Key);
+
+            string res = "";
+            foreach (var item in de)
+            {
+                res = res + item.Value.ToString();
+                res = res + " ";
+            }
+           return res.Trim();
+        }
     }
 }
