73d499f743ec4f7ce5299ee43fc1ec2f8c243841
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-10-04 20:53:34 +0100

    UtilitySnippets

---
+++ CodingTasks/UtilitySnippets/UtilitySnippets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks.UtilitySnippets
+{
+    internal class UtilitySnippets
+    {
+    }
+
+    public class DateTimeProp
+    {
+        public string StartDate { get; set; }
+    }
+}
