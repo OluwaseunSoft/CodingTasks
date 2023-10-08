0a3cd669e9f6485d19f9f5ad79438c1aacece441
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-10-08 22:47:15 +0100

    MiniMaxSum

---
+++ CodingTasks/MiniMaxSum/MiniMaxSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks.MiniMaxSum
+{
+    internal class MiniMaxSum
+    {
+        public static void MiniMaxSumFunction(int[] arr)
+        {
+
+        }
+    }
+}
