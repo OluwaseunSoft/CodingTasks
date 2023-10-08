dfa034ba3bc062dfd4572c0b1366e6a484cf701d
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-10-08 23:09:28 +0100

    Done

--- CodingTasks/Program.cs
+++ CodingTasks/Program.cs
@@ -35,4 +35,4 @@ using CodingTasks.UtilitySnippets;
 
 //PositiveNegativeZeroArray.PositiveNegativeZeroArrayMethod(new List<int> { -9, 0, 8, 6, -4 , -8});
 
-MiniMaxSum.MiniMaxSumFunction(new int[] { 1, 2, 3, 4, 5 });
+MiniMaxSum.MiniMaxSumFunction(new int[] { 1, 3, 5, 7, 9 });
