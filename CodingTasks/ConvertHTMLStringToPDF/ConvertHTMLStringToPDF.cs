852b698fc7c380f3bd784cc0a731839fe0f28e0a
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-10-14 20:34:09 +0100

    ConvertHTMLStringToPDF

---
+++ CodingTasks/ConvertHTMLStringToPDF/ConvertHTMLStringToPDF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks.ConvertHTMLStringToPDF
+{
+    internal class ConvertHTMLStringToPDF
+    {
+        public static void ConvertHTML2PDF()
+        {
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
