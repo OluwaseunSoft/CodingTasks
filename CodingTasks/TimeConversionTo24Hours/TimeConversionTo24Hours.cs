2b4537809ea213cce5d90028f740b269cde1fb57
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2023-10-31 20:19:37 +0100

    TimeConversionTo24Hours

---
+++ CodingTasks/TimeConversionTo24Hours/TimeConversionTo24Hours.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTasks.TimeConversionTo24Hours
+{
+    public class TimeConversionTo24Hours
+    {
+        public string TimeConversion(string time)
+        {
+            return time;
+        }
+    }
+}
